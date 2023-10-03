96e4b5beb8bc0b809aec0fdfeb9b6e43f567bf6d
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-03 08:26:26 +0200

    Add api to project

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/Controllers/GamesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pri.GameLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+    }
+}
