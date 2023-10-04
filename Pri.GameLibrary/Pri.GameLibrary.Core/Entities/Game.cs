010c03c8810d7d444c5a008d9e9c878a060bbf68
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:38:46 +0200

    Create game entity

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Pri.GameLibrary.Core.Entities
+{
+    public class Game : BaseEntity
+    {
+        public string Name { get; set; }
+        public int DeveloperId { get;set; }
+        public Developer Developer { get; set; }
+        public IEnumerable<Console> Consoles { get; set; }
+        public IEnumerable<GameUser> Users { get; set; }
+    }
+}
