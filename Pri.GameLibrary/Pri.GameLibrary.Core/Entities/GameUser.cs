de6a8035f9b694bb06c40c180f33700a223f967c
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:38:58 +0200

    Create gameUser entity

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/GameUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Entities
+{
+    public class GameUser
+    {
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+        public Game Game { get; set; }
+        public User User { get; set; }
+        public int ReviewId { get; set; }
+        public Review Review { get; set; }
+
+    }
+}
