4692a568385795a63cd582b8c070b7fe0996e42d
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:39:14 +0200

    Create user entity

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Pri.GameLibrary.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public IEnumerable<GameUser> Games { get; set; }
+        public string Email { get; set; }
+    }
+}
