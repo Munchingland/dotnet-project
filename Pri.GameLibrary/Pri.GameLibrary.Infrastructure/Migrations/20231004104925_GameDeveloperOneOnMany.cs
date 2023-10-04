d79f9f2c2bd232b46615f35caa5b57a5b287f5f2
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 12:50:26 +0200

    Add one on many on game and developer relationship

---
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Migrations/20231004104925_GameDeveloperOneOnMany.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pri.GameLibrary.Infrastructure.Migrations
+{
+    public partial class GameDeveloperOneOnMany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
