8ff741df8cbb97c4d6131e1f3c6b8d1055675f57
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:39:06 +0200

    Create review entity

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Entities
+{
+    public class Review : BaseEntity
+    {
+        public string Description { get; set; }
+        public int Rating { get; set; }
+    }
+}
