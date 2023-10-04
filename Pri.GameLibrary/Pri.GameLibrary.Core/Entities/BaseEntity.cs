bd7302cd4941fbd19c454eb3e58a6d784679d974
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:18:53 +0200

    Update baseEntity

--- Pri.GameLibrary/Pri.GameLibrary.Core/Entities/BaseEntity.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/BaseEntity.cs
@@ -8,6 +8,9 @@ namespace Pri.GameLibrary.Core.Entities
 {
     public class BaseEntity
     {
-
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Deleted { get; set; }
     }
 }
