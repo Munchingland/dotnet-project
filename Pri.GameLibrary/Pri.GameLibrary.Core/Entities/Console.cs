11c89ac61bc021055284a70c2066f5e0f9929c26
Jarno Caenepeel <Jarno.caenepeel@student.howest.be>
2023-10-04 11:38:20 +0200

    Create console entity

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Entities/Console.cs
@@ -0,0 +1,8 @@
+﻿namespace Pri.GameLibrary.Core.Entities
+{
+    public class Console : BaseEntity
+    {
+        public string Name { get; set; }
+        public IEnumerable<Game> Games { get; set; }
+    }
+}
