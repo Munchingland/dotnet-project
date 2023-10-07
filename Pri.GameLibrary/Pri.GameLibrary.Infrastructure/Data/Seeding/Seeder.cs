7228b42c80f656d78bd1f54567ac919714eed38f
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-07 14:29:39 +0200

    Add seeddata

--- Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Data/Seeding/Seeder.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Data/Seeding/Seeder.cs
@@ -69,7 +69,77 @@ namespace Pri.GameLibrary.Infrastructure.Data.Seeding
                     Name = "Santa Monica Studios",
                     Id= 5
                 });
-
+            modelBuilder.Entity<Game>().HasData(
+                new Game()
+                {
+                    Created = new DateTime(2021, 08, 25),
+                    Id = 1,
+                    Name = "Psychonauts 2",
+                    DeveloperId = 2,
+                },
+                new Game()
+                {
+                    Created = new DateTime(2018,04,20),
+                    Id = 2,
+                    Name = "God of War",
+                    DeveloperId = 5
+                },
+                new Game()
+                {
+                    Created = new DateTime(2018, 05, 23),
+                    Id = 3,
+                    Name = "Dark Souls: Remastered",
+                    DeveloperId = 3
+                },
+                new Game()
+                {
+                    Created = new DateTime(2022, 02, 25),
+                    Id = 4,
+                    Name = "Elden Ring",
+                    DeveloperId = 3
+                },
+                new Game()
+                {
+                    Created = new DateTime(2015, 03, 24),
+                    Id = 5,
+                    Name = "Bloodborne",
+                    DeveloperId = 3
+                },
+                new Game()
+                {
+                    Created = new DateTime(2022, 11, 18),
+                    Id=6,
+                    Name= "Pokemon Scarlet",
+                    DeveloperId = 4
+                },
+                new Game()
+                {
+                    Created = new DateTime(2022, 11, 18),
+                    Id = 7,
+                    Name = "Pokemon Violet",
+                    DeveloperId = 4
+                },
+                new Game()
+                {
+                    Created = new DateTime(2019, 11, 15),
+                    Id = 8,
+                    Name = "Pokemon Sword",
+                    DeveloperId = 4
+                },
+                new Game()
+                {
+                    Created = new DateTime(2019, 11, 15),
+                    Id = 9,
+                    Name = "Pokemon Shield",
+                    DeveloperId = 4
+                },
+                new Game()
+                {
+                    Created = new DateTime(2023, 09, 6),
+                    Id = 10,
+                    Name = "Starfield",
+                    DeveloperId = 1
+                });
             
         }
     }
