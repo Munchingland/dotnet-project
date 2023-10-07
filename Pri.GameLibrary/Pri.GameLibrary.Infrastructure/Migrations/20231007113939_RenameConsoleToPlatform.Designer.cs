1eb7d59a964cdef9c8ddb3152662cbfd4adb8df7
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-07 13:56:56 +0200

    Create migration for name change

--- Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Migrations/20231007113939_RenameConsoleToPlatform.Designer.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Migrations/20231007113939_RenameConsoleToPlatform.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pri.GameLibrary.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using Pri.GameLibrary.Infrastructure.Data;
 namespace Pri.GameLibrary.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231007113939_RenameConsoleToPlatform")]
+    partial class RenameConsoleToPlatform
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
