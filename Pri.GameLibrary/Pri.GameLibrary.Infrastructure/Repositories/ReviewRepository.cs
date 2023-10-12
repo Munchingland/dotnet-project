c7007a3fb3fe08d83fa90e9ab5b602862b6493f7
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:55:20 +0200

    Create review repo

---
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Repositories/ReviewRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
+using Pri.GameLibrary.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Infrastructure.Repositories
+{
+    public class ReviewRepository : BaseRepository<Review>, IReviewRepository
+    {
+        public ReviewRepository(ApplicationDbContext applicationDbContext, ILogger<BaseRepository<Review>> logger) : base(applicationDbContext, logger)
+        {
+        }
+
+        public Task<IEnumerable<Review>> GetByGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
