2bcdfa7064a688958dd0313ac4bec9007a163276
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:43:03 +0200

    Add extra methods to game repo interface

--- Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Repositories/IGameRepository.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Repositories/IGameRepository.cs
@@ -9,5 +9,7 @@ namespace Pri.GameLibrary.Core.Interfaces.Repositories
 {
     public interface IGameRepository:IBaseRepository<Game>
     {
+        Task<IEnumerable<Game>> GetByDeveloper(int id);
+        Task<IEnumerable<Game>> GetByConsole(int id);
     }
 }
