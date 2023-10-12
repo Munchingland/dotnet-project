871ff1db5251360cb0d0bca4c88b072db8bf8531
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:55:11 +0200

    Create platform repo

---
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Repositories/PlatformRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
+using Pri.GameLibrary.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Infrastructure.Repositories
+{
+    public class PlatformRepository : BaseRepository<Platform>, IPlatformRepository
+    {
+        public PlatformRepository(ApplicationDbContext applicationDbContext, ILogger<BaseRepository<Platform>> logger) : base(applicationDbContext, logger)
+        {
+        }
+    }
+}
