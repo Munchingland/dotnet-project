21f971d5412890bb190e2c9f4a830ca19dd29468
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:40:16 +0200

    Create game repo interface

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Repositories/IGameRepository.cs
@@ -0,0 +1,13 @@
+﻿using Pri.GameLibrary.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Interfaces.Repositories
+{
+    public interface IGameRepository:IBaseRepository<Game>
+    {
+    }
+}
