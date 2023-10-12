1d258eccd87b92865510ec6cfbf7c3cb47d30443
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:42:04 +0200

    Create review repo interface

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Repositories/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pri.GameLibrary.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Interfaces.Repositories
+{
+    public interface IReviewRepository : IBaseRepository<Review>
+    {
+        Task<IEnumerable<Review>> GetByGame(int id);
+    }
+}
