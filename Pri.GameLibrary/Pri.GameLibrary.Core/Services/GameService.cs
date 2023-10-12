047e67a3e2a21c386c6fe2efdc986713204383d2
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 14:35:13 +0200

    Implement create, delete, exists and get all from game service interface in game service

--- Pri.GameLibrary/Pri.GameLibrary.Core/Services/GameService.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Services/GameService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
+using Pri.GameLibrary.Core.Interfaces.Services;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,114 @@ using System.Threading.Tasks;
 
 namespace Pri.GameLibrary.Core.Services
 {
-    internal class GameService
+    public class GameService : IGameService
     {
+        private readonly IGameRepository _gameRepository;
+        private readonly IPlatformRepository _platformRepository;
+        private readonly IDeveloperRepository _developerRepository;
+
+        public GameService(IPlatformRepository platformRepository, IDeveloperRepository developerRepository, IGameRepository gameRepository)
+        {
+            _platformRepository = platformRepository;
+            _developerRepository = developerRepository;
+            _gameRepository = gameRepository;
+        }
+
+        public async Task<ResultModel<Game>> CreateAsync(string name, int developerId, IEnumerable<int> platformIds, DateTime releaseDate)
+        {
+            var platforms = _platformRepository.GetAll();
+            if(!platformIds.All(pl => platforms.Any(p => p.Id == pl)))
+            {
+                return new ResultModel<Game>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Unkown Platform" }
+                };
+            }
+            var developer = _developerRepository.GetAll();
+            if(!await developer.AnyAsync(d=>d.Id == developerId))
+            {
+                return new ResultModel<Game>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Unkown Developer" }
+                };
+            }
+            if( await _gameRepository.GetAll().AnyAsync(g => g.Name.ToUpper().Equals(name.ToUpper())))
+            {
+                return new ResultModel<Game>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Name exists!" }
+                };
+            }
+            var game = new Game
+            {
+                Name = name,
+                Created = releaseDate,
+                Platforms = _platformRepository.GetAll().Where(p => platformIds.Contains(p.Id)).ToList(),
+                DeveloperId = developerId,
+            };
+            if(!await _gameRepository.CreateAsync(game))
+            {
+                return new ResultModel<Game>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Unknown error, please try again later..." }
+                };
+            }
+            return new ResultModel<Game>
+            {
+                IsSuccess = true
+            };
+
+        }
+
+        public async Task<ResultModel<Game>> DeleteAsync(int id)
+        {
+            var toDelete = await _gameRepository.GetByIdAsync(id);
+            if(!await _gameRepository.DeleteAsync(toDelete))
+            {
+                return new ResultModel<Game>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Unknown error, please try again later..." }
+                };
+            }
+            return new ResultModel<Game>
+            {
+                IsSuccess = true
+            };
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _gameRepository.GetAll().AnyAsync(g=>g.Id == id);
+        }
+
+        public async Task<ResultModel<Game>> GetAllAsync()
+        {
+            var games = await _gameRepository.GetAllAsync();
+            return new ResultModel<Game>
+            {
+                IsSuccess = true,
+                Items = games
+            };
+        }
+
+        public Task<ResultModel<Game>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Game>> SearchByNameAsync(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Game>> UpdateAsync(int id, string name, int developerId, IEnumerable<int> platformIds)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
