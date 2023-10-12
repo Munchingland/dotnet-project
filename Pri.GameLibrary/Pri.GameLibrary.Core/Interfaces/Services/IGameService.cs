5d70e0a321932904a58107b563e4f61564999a4d
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 14:12:01 +0200

    Create game service interface + resultmodel

--- Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Services/IGameService.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Services/IGameService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,15 @@ using System.Threading.Tasks;
 
 namespace Pri.GameLibrary.Core.Interfaces.Services
 {
-    internal class IGameService
+    public interface IGameService
     {
+        Task<ResultModel<Game>> GetAllAsync();
+        Task<ResultModel<Game>> GetByIdAsync(int id);
+        Task<ResultModel<Game>> CreateAsync();
+        Task<ResultModel<Game>> UpdateAsync(int id);
+        Task<ResultModel<Game>> SearchByNameAsync(string search);
+        Task<bool> ExistsAsync(int id);
+        Task<ResultModel<Game>> DeleteAsync(int id);
+
     }
 }
