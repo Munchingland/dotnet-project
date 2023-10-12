74958e16c79286399c542e37ecdf8ba036f52d92
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-12 13:55:01 +0200

    Create game repo

---
+++ Pri.GameLibrary/Pri.GameLibrary.Infrastructure/Repositories/GameRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
+using Pri.GameLibrary.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Infrastructure.Repositories
+{
+    public class GameRepository : BaseRepository<Game>, IGameRepository
+    {
+        public GameRepository(ApplicationDbContext applicationDbContext, ILogger<BaseRepository<Game>> logger) : base(applicationDbContext, logger)
+        {
+        }
+
+        public Task<IEnumerable<Game>> GetByConsole(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Game>> GetByDeveloper(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
