ed56a38305226cefd845a22cf277858cf18d2962
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-13 12:10:21 +0200

    Create gamesGetByIdDto

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Response/GamesGetByIdDto.cs
@@ -0,0 +1,16 @@
+﻿using Pri.GameLibrary.Core.Entities;
+
+namespace Pri.GameLibrary.Api.DTOs.Response
+{
+    public class GamesGetByIdDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AverageReview { get; set; }
+        public int DeveloperID { get; set; }
+        public Developer Developer { get; set; }
+        public ICollection<Platform> Platforms { get; set; }
+        public ICollection<int> PlatformIds { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
