61273e7c65880007964477387b5f76aa8ce8e1c2
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-13 15:44:49 +0200

    Create developer service

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Services/IDeveloperService.cs
@@ -0,0 +1,16 @@
+﻿using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Interfaces.Services
+{
+    public interface IDeveloperService : IBaseService<Developer>
+    {
+        Task<ResultModel<Developer>> CreateAsync(string name, DateTime creationDate);
+        Task<ResultModel<Developer>> UpdateAsync(int id, string name, DateTime creationDate);
+    }
+}
---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Services/DeveloperService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
+using Pri.GameLibrary.Core.Interfaces.Services;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Services
+{
+    public class DeveloperService : IDeveloperService
+    {
+        private readonly IDeveloperRepository _developerRepository;
+
+        public DeveloperService( IDeveloperRepository developerRepository)
+        {
+            _developerRepository = developerRepository;
+        }
+
+        public async Task<ResultModel<Developer>> CreateAsync(string name, DateTime creationDate)
+        {
+            if(await _developerRepository.GetAll().AnyAsync(d => d.Name.ToUpper().Equals(name.ToUpper())))
+            {
+                return new ResultModel<Developer>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Name exists!" }
+                };
+            }
+            var developer = new Developer
+            {
+                Name = name,
+                Created = creationDate,
+            };
+            if(!await _developerRepository.CreateAsync(developer)) 
+            {
+                return new ResultModel<Developer>
+                {
+                    IsSuccess = false,
+                    Errors = new List<string>() { "Unknown error, please try again later..." }
+                };
+            }
+            return new ResultModel<Developer>
+            {
+                IsSuccess = true
+            };
+        }
+
+        public Task<ResultModel<Game>> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Developer>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Developer>> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Developer>> SearchByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultModel<Developer>> UpdateAsync(int id, string name, DateTime creationDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
