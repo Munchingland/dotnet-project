6805fbf1623c17a22c35d7ec91b6922a8ad498e6
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-13 15:22:12 +0200

    Create base service interface

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Services/IBaseService.cs
@@ -0,0 +1,19 @@
+﻿using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Interfaces.Services
+{
+    public interface IBaseService<T> where T : BaseEntity
+    {
+        Task<ResultModel<T>> GetAllAsync();
+        Task<ResultModel<T>> GetByIdAsync(int id);
+        Task<ResultModel<T>> SearchByNameAsync(string name);
+        Task<bool> ExistsAsync(int id);
+        Task<ResultModel<Game>> DeleteAsync(int id);
+    }
+}
