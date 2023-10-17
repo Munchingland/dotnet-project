2453138d264b0b61703768976b02e7831d58c7ed
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-17 20:34:08 +0200

    Create platform controller

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/Controllers/PlatformsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pri.GameLibrary.Api.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
