aca74243be48cd87c51d3cc067654f96de5646b8
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-18 21:07:34 +0200

    Restructure gamesGetByIdViewModel

--- Pri.GameLibrary/Pri.GameLibrary.Web/ViewModels/GamesGetByIdViewModel.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Web/ViewModels/GamesGetByIdViewModel.cs
@@ -3,8 +3,8 @@
     public class GamesGetByIdViewModel:BaseViewModel
     {
         public double AverageReview { get; set; }
-        public string Developer { get; set; }
-        public IEnumerable<string> Platforms { get; set; }
+        public BaseViewModel Developer { get; set; }
+        public IEnumerable<BaseViewModel> Platforms { get; set; }
         public string ReleaseDate { get; set; }
     }
 }
