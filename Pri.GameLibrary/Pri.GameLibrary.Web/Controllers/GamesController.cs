4e6fd880fd86a9a63612307222c387a0bcf0bc5b
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-18 19:55:40 +0200

    Set up api connection

---
+++ Pri.GameLibrary/Pri.GameLibrary.Web/Controllers/GamesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pri.GameLibrary.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly string _baseUrl;
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public GamesController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _configuration = configuration;
+            _baseUrl = $"{_configuration.GetSection("ApiUrl:BaseUrl").Value}/Drinks";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
