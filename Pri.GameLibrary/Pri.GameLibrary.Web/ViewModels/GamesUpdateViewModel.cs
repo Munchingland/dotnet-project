1bd233b912257f07e5760542e0aa0ca16ff5a468
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-19 01:41:09 +0200

    Create game update view model

---
+++ Pri.GameLibrary/Pri.GameLibrary.Web/ViewModels/GamesUpdateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pri.GameLibrary.Web.ViewModels
+{
+    public class GamesUpdateViewModel:GamesAddViewModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+    }
+}
