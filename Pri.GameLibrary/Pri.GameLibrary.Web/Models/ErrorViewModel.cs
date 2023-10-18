1e4fbbaf65463a52f203c9bb052528504b37f67a
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-10-18 19:43:02 +0200

    Add mvc application

---
+++ Pri.GameLibrary/Pri.GameLibrary.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pri.GameLibrary.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
