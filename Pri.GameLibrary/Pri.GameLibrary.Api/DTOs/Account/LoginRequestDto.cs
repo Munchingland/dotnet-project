d9db2bc51ac87629712208d031a41461a8cd5ba1
JarnoHowest <Jarno.caenepeel@student.howest.be>
2023-11-10 16:15:32 +0100

    Create dto for login

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Account/LoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pri.GameLibrary.Api.DTOs.Account
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
