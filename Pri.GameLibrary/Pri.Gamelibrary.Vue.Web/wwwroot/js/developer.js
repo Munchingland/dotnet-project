d2718f6e76b180b122a7077362c8499fd22be2d3
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-02 14:37:12 +0100

    Get basic info of developers

---
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/developer.js
@@ -0,0 +1,26 @@
+﻿let app = new Vue({
+    el: "#developer",
+    data: {
+        baseUrl: "https://localhost:7056/api",
+        loading: false,
+        developers: [],
+        errorMessage: "",
+        hasError: false,
+        developersVisible: false,
+    },
+    methods: {
+        showDevelopers: async function () {
+            this.loading = true;
+            this.developers = await axios.get(`${this.baseUrl}/Developers`)
+                .then(response => response.data.items)
+                .catch(error => {
+                    if (error.response.status == 404) {
+                        this.errorMessage = "endpoint not found";
+                    }
+                    this.hasError = true;
+                });
+            this.developersVisible = true;
+            this.loading = false;
+        },
+    },
+});
