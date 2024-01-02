2a1fe2841e68d87a19d9cf4c4095db7f08d27109
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-02 13:03:00 +0100

    Add correct vue project

---
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pri.Gamelibrary.Vue.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
