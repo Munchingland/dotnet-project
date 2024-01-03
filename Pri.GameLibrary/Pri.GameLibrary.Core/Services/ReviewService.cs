8239aa10b5238339017fa7b55e41818c37c36036
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 15:24:14 +0100

    get review by game id in service

--- Pri.GameLibrary/Pri.GameLibrary.Core/Services/ReviewService.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Services/ReviewService.cs
@@ -1,5 +1,7 @@
-﻿using Pri.GameLibrary.Core.Interfaces.Repositories;
+﻿using Pri.GameLibrary.Core.Entities;
+using Pri.GameLibrary.Core.Interfaces.Repositories;
 using Pri.GameLibrary.Core.Interfaces.Services;
+using Pri.GameLibrary.Core.Interfaces.Services.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,7 @@ namespace Pri.GameLibrary.Core.Services
 
         public async Task<double> GetAverageScoreAsync(int id)
         {
-            var reviews = await _reviewRepository.GetByGame(id);
+            var reviews = await _reviewRepository.GetByGameAsync(id);
             if(reviews.Count() == 0)
             {
                 return 0;
@@ -32,6 +34,16 @@ namespace Pri.GameLibrary.Core.Services
                 }
             }
             return reviews.Average(r => r.Rating);
+        }
+
+        public async Task<ResultModel<Review>> GetByGameIdAsync(int id)
+        {
+            var result = await _reviewRepository.GetByGameAsync(id);
+            return new ResultModel<Review>
+            {
+                IsSuccess = true,
+                Items = result
+            };
         }
     }
 }
