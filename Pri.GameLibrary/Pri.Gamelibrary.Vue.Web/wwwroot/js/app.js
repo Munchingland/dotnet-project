d42b53f55ac4b0bf514ff5bf40fbfed598cce073
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 15:24:43 +0100

    Display reviews when choosing to

--- Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/app.js
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/app.js
@@ -7,10 +7,14 @@
         errorMessage: "",
         hasError: false,
         gamesVisible: false,
+        reviews: [],
+        reviewsVisible: false,
+        gameName: ""
     },
     methods: {
         showGames: async function () {
             this.loading = true;
+            this.reviewsVisible = false;
             this.games = await axios.get(`${this.baseUrl}/Games`)
                 .then(response => response.data.items)
                 .catch(error => {
@@ -21,6 +25,23 @@
                 });
             this.gamesVisible = true;
             this.loading = false;
+            
         },
+        showReviews: async function (game) {
+            this.loading = true;
+            this.games = [];
+            this.gameName = game.name;
+            this.games.push(game);
+            this.reviews = await axios.get(`${this.baseUrl}/Reviews/${game.id}`)
+                .then(response => response.data.items)
+                .catch(error => {
+                    if (error.response.status == 404) {
+                        this.errorMessage = "endpoint not found";
+                    }
+                    this.hasError = true;
+                })
+            this.reviewsVisible = true;
+            this.loading = false;
+        }
     },
 });
