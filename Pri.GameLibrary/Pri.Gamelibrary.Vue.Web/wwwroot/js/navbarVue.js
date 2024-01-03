bf8e9aa8597d942d2eb60d340d34de6be35c0819
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 19:20:41 +0100

    Login in on site

---
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/navbarVue.js
@@ -0,0 +1,49 @@
+﻿let navbarVue = new Vue({
+    el: ".navbar",
+    name: "navigation",
+    data: {
+        loggedIn: false,
+        loginModel: {
+            email: null,
+            password: null,
+        },
+        error: false,
+        token: null,
+        isAdmin: false,
+        userName : "",
+        
+       
+        errorMessage: "",
+    },
+    created: function () {
+        if (sessionStorage.getItem("token")) {
+            this.loggedIn = true;
+            this.isAdmin = hasUserAdminRole();
+            this.userName = readUserNameFromToken();
+        }
+        else {
+            this.loggedIn = false;
+        }
+    },
+    methods: {
+        submitLogin: async function () {
+            await axios.post(`${baseUrl}/auth/login`, this.loginModel)
+                .then((response) => {
+                    sessionStorage.setItem("token", response.data.bearerToken);
+                    this.loggedIn = true;
+                    this.userName = readUserNameFromToken();
+                    this.isAdmin = hasUserAdminRole();
+                    this.errorMessage = null;
+                }).
+                catch((e) => {
+                    this.errorMessage = "Login failed!";
+                    this.loggedIn = false;
+                })
+                .finally(() => {
+                    this.loginModel.email = "";
+                    this.loginModel.password = ""
+                });
+        },
+
+    }
+});
