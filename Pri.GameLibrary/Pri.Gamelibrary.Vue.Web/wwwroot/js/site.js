24aae5e1cdacf5cce96e48e3ce0c047f82c85ca3
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 19:18:46 +0100

    Add code to site for decoding token and keeping baseUrl

--- Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/site.js
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/site.js
@@ -1,4 +1,57 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const baseUrl = "https://localhost:7056/api";
 
-// Write your JavaScript code.
+//Keys of ClaimTypes
+const roleClaimTypeKey = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+const userIdClaimTypeKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/primarysid";
+const nameClaimTypeKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name";
+const dateOfBirthClaimTypeKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/dateofbirth";
+const emailClaimTypeKey = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress";
+
+//Values of ClaimTypes
+const adminRoleClaimTypeValue = "Admin";
+
+// Axios configuration
+let axiosConfig = {
+    headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` }
+};
+
+//Methods
+function getDecodedToken() {
+    const token = sessionStorage.getItem("token");
+    if (token == null) {
+        return "";
+    }
+    else {
+        const decodedToken = jwt_decode(token);
+        return decodedToken;
+    }
+}
+
+function readUserNameFromToken() {
+    const decodedToken = getDecodedToken();
+    return decodedToken[nameClaimTypeKey];
+}
+
+function readUserIdFromToken() {
+    const decodedToken = getDecodedToken();
+    return decodedToken[userIdClaimTypeKey];
+}
+
+function readUserBirthDayFromToken() {
+    const decodedToken = getDecodedToken();
+    return decodedToken[dateOfBirthClaimTypeKey];
+}
+
+function readUserRoleFromToken() {
+    const decodedToken = getDecodedToken();
+    return decodedToken[roleClaimTypeKey];
+}
+
+function hasUserAdminRole() {
+    if (readUserRoleFromToken() === adminRoleClaimTypeValue) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
