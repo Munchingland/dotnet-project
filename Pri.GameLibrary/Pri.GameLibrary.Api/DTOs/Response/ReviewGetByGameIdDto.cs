ddb0fceff31ee3fe1ad710f7cbf9dbe8a3019cc9
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 15:23:07 +0100

    Add needed Dto's

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Response/ReviewGetByGameIdDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Pri.GameLibrary.Api.DTOs.Response
+{
+    public class ReviewGetByGameIdDto
+    {
+        public IEnumerable<ReviewGetByIdDto> Reviews { get; set; }
+    }
+}
---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Response/ReviewGetByIdDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Pri.GameLibrary.Api.DTOs.Response
+{
+    public class ReviewGetByIdDto 
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
