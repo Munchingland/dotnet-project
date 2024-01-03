ab4dd6bb4e7942df679f518e19e5ec9401ff6055
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-03 15:22:56 +0100

    Add review controller

---
+++ Pri.GameLibrary/Pri.GameLibrary.Api/Controllers/ReviewsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pri.GameLibrary.Api.DTOs.Response;
+using Pri.GameLibrary.Core.Interfaces.Services;
+
+namespace Pri.GameLibrary.Api.Controllers
+{
+    [Route("Api/[controller]")]
+    [ApiController]
+    public class ReviewsController : Controller
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewsController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetByGameId(int id)
+        {
+            var result = await _reviewService.GetByGameIdAsync(id);
+            var reviewGetByGameIdDto = new ReviewGetByGameIdDto
+            {
+                Reviews = result.Items.Select(r => new ReviewGetByIdDto
+                {
+                    Created = r.Created,
+                    Description = r.Description,
+                    Id = r.Id,
+                    Rating = r.Rating,
+                })
+            };
+            return Ok(reviewGetByGameIdDto);
+        }
+    }
+}
