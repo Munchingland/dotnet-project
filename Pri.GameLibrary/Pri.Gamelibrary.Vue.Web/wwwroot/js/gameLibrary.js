b0c761703f61b4335b07f82b5c1c2073a9d156ab
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-04 15:51:27 +0100

    Make vue file only for gameLibrary

---
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/gameLibrary.js
@@ -0,0 +1,77 @@
+﻿let gameLibrary = new Vue({
+    el: "#games",
+    data: {
+        loading: false,
+        games: [],
+        errorMessage: "",
+        hasError: false,
+        gamesVisible: false,
+        reviews: [],
+        reviewsVisible: false,
+        gameName: "",
+        userId: "",
+
+        userGamesLoaded: false,
+    },
+
+    mounted: async function () {
+        if (sessionStorage.getItem("token")) {
+            await this.showGamesOwnedByUser();
+        }
+        else {
+            await this.showGames();
+        }
+
+    },
+
+    methods: {
+        showGamesNotOwnedByUser: async function () {
+            if (sessionStorage.getItem("token")) {
+                this.userId = readUserIdFromToken();
+                this.loading = true;
+                this.reviewsVisible = false;
+
+                this.games = await axios.get(`${baseUrl}/games/NotUser/${this.userId}`)
+                    .then(response => response.data.items)
+                    .catch(error => {
+                        if (error.response.status == 404) {
+                            this.errorMessage = "endpoint not found";
+                        }
+                        this.hasError = true;
+                    });
+                this.userGamesLoaded = false;
+                this.gamesVisible = true;
+                this.loading = false;
+
+
+            }
+            else {
+                this.errorMessage = "Please login before seeing library";
+            }
+        },
+        showGamesOwnedByUser: async function () {
+            if (sessionStorage.getItem("token")) {
+                this.userId = readUserIdFromToken();
+                this.loading = true;
+                this.reviewsVisible = false;
+
+                this.games = await axios.get(`${baseUrl}/games/user/${this.userId}`)
+                    .then(response => response.data.items)
+                    .catch(error => {
+                        if (error.response.status == 404) {
+                            this.errorMessage = "endpoint not found";
+                        }
+                        this.hasError = true;
+                    });
+                this.userGamesLoaded = true;
+                this.gamesVisible = true;
+                this.loading = false;
+
+
+            }
+            else {
+                this.errorMessage = "Please login before seeing library";
+            }
+        },
+    },
+});
