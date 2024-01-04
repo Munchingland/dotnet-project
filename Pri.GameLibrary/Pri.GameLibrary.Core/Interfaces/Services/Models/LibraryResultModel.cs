f9a51a9ccbfd378e9192aefc5103ec87b4587a83
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-04 19:56:47 +0100

    Add library result model

---
+++ Pri.GameLibrary/Pri.GameLibrary.Core/Interfaces/Services/Models/LibraryResultModel.cs
@@ -0,0 +1,16 @@
+﻿using Pri.GameLibrary.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.GameLibrary.Core.Interfaces.Services.Models
+{
+    public class LibraryResultModel
+    {
+        public bool IsSuccess { get; set; }
+        public IEnumerable<GameUser> Items { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
