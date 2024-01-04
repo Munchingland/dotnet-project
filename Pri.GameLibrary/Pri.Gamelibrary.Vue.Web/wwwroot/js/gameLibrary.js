3a53986822ca148baa4bf32c5ebbbb98c18fcbf7
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-04 19:59:17 +0100

    Remove and add game to library

--- Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/gameLibrary.js
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/gameLibrary.js
@@ -11,27 +11,25 @@
         gameName: "",
         userId: "",
 
+        libraryAddModel:{
+            gameId: "",
+            userId: ""
+        },
+
         userGamesLoaded: false,
     },
 
     mounted: async function () {
-        if (sessionStorage.getItem("token")) {
-            await this.showGamesOwnedByUser();
-        }
-        else {
-            await this.showGames();
-        }
+        await this.showGamesOwnedByUser();
 
     },
 
     methods: {
         showGamesNotOwnedByUser: async function () {
             if (sessionStorage.getItem("token")) {
-                this.userId = readUserIdFromToken();
-                this.loading = true;
-                this.reviewsVisible = false;
+                this.setDataWhenStartingFunction();
 
-                this.games = await axios.get(`${baseUrl}/games/NotUser/${this.userId}`)
+                this.games = await axios.get(`${baseUrl}/library/NotUser/${this.userId}`)
                     .then(response => response.data.items)
                     .catch(error => {
                         if (error.response.status == 404) {
@@ -40,22 +38,18 @@
                         this.hasError = true;
                     });
                 this.userGamesLoaded = false;
-                this.gamesVisible = true;
-                this.loading = false;
 
-
+                this.setDataWhenFinishingFunction();
             }
             else {
-                this.errorMessage = "Please login before seeing library";
+                this.setErrorMessageNotLoggedIn();
             }
         },
         showGamesOwnedByUser: async function () {
             if (sessionStorage.getItem("token")) {
-                this.userId = readUserIdFromToken();
-                this.loading = true;
-                this.reviewsVisible = false;
+                this.setDataWhenStartingFunction();
 
-                this.games = await axios.get(`${baseUrl}/games/user/${this.userId}`)
+                this.games = await axios.get(`${baseUrl}/library/user/${this.userId}`)
                     .then(response => response.data.items)
                     .catch(error => {
                         if (error.response.status == 404) {
@@ -64,14 +58,64 @@
                         this.hasError = true;
                     });
                 this.userGamesLoaded = true;
-                this.gamesVisible = true;
-                this.loading = false;
 
-
+                this.setDataWhenFinishingFunction();
             }
             else {
-                this.errorMessage = "Please login before seeing library";
+                this.setErrorMessageNotLoggedIn();
             }
+        },
+        addToLibrary: async function (game) {
+            if (sessionStorage.getItem("token")) {
+                this.setDataWhenStartingFunction();
+                this.libraryAddModel.userId = this.userId;
+                this.libraryAddModel.gameId = game.id;
+
+                await axios.post(`${baseUrl}/library`, this.libraryAddModel)
+                    .catch(error => {
+                        if (error.response.status == 404) {
+                            this.errorMessage = "endpoint not found";
+                        }
+                        this.hasError = true;
+                    });
+
+                await this.showGamesNotOwnedByUser();
+            }
+            else {
+                this.setErrorMessageNotLoggedIn();
+            }
+        },
+        removeFromLibrary: async function (game) {
+            if (sessionStorage.getItem("token")) {
+                this.setDataWhenStartingFunction();
+
+                await axios.delete(`${baseUrl}/library/${this.userId}/${game.id}`)
+                    .catch(error => {
+                        if (error.response.status == 404) {
+                            this.errorMessage = "endpoint not found";
+                        }
+                        this.hasError = true;
+                    });
+
+
+                await this.showGamesOwnedByUser();
+            }
+            else {
+                this.setErrorMessageNotLoggedIn();
+            }
+        },
+        setDataWhenStartingFunction: function () {
+            this.userId = readUserIdFromToken();
+            this.loading = true;
+            this.reviewsVisible = false;
+        },
+        setDataWhenFinishingFunction: function () {
+            this.gamesVisible = true;
+            this.loading = false;
+        },
+
+        setErrorMessageNotLoggedIn: function () {
+            this.errorMessage = "Please login before seeing library";
         },
     },
 });
