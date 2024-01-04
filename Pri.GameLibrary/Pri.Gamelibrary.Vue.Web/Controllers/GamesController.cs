d0911608b3caa593bbbacd74b8fda7a221fb85e8
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-04 14:43:48 +0100

    Create game controller

---
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/Controllers/GamesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pri.Gamelibrary.Vue.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
