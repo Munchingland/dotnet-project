c2185735a16c0ce223de4994998271d7beb9a239
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-08 19:59:25 +0100

    Add crud for platforms

--- Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/platform.js
+++ Pri.GameLibrary/Pri.Gamelibrary.Vue.Web/wwwroot/js/platform.js
@@ -6,16 +6,35 @@
         errorMessage: "",
         hasError: false,
         platformsVisible: false,
+        selectedPlatform: {},
+
+        newPlatformModel: {
+            name: "",
+            creationDate: new Date(),
+
+        },
+
+        updatePlatformModel : {
+            name: "",
+            id: 0,
+            creationDate: new Date(),
+        },
+        gamePage: false,
+        isAdmin:false,
 
         gamesVisible: false,
         games : [],
     },
+
     mounted: async function () {
         await this.showPlatforms();
     },
+
     methods: {
         showPlatforms: async function () {
             this.loading = true;
+            this.isAdmin = hasUserAdminRole();
+
             this.gamesVisible = false;
             this.platforms = await axios.get(`${baseUrl}/Platforms`)
                 .then(response => response.data.items)
@@ -28,6 +47,7 @@
             this.platformsVisible = true;
             this.loading = false;
         },
+
         showGamesOnPlatform: async function (platform) {
             this.loading = true;
             this.platforms = [];
@@ -42,6 +62,101 @@
                 });
             this.gamesVisible = true;
             this.loading = false;
+        },
+
+        deletePlatform: async function (selectedPlatformId) {
+            this.isLoading = true;
+            await axios.delete(`${baseUrl}/platforms/${selectedPlatformId}`, axiosConfig).catch((e) => {
+                if (e.response.status === 400) {
+                    console.log(e.response.data[0].errorMessage);
+                    this.errorMessage = e.response.data[0].errorMessage;
+                }
+                else {
+                    this.errorMessage = e.message;
+                }
+            });
+            $('#deletePlatformConfirmModal').modal('hide');
+            this.selectedPlatform = {};
+            this.showPlatforms();
+        },
+        createPlatform: async function () {
+            this.newPlatformModel.name.trim();
+            this.loading = true;
+            await axios.post(`${baseUrl}/Platforms`, this.newPlatformModel, axiosConfig)
+                .catch(e => {
+                    hasError = true;
+                    if (e.response.status === 401) {
+                        this.errorMessage = "Does not posses the required rights to perform this action";
+                    }
+                    else if (e.response.status == 400) {
+                        this.errorMessage = e.response.data[0].errorMessage;
+                    }
+                    else {
+                        this.errorMessage = e.message;
+                    }
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            $('#createPlatformModal').modal('hide');
+            this.resetPlatformModels();
+            this.showPlatforms();
+        },
+        updatePlatform: async function () {
+            this.updatePlatformModel.name.trim();
+            this.loading = true;
+            await axios.put(`${baseUrl}/Platforms`, this.updatePlatformModel, axiosConfig)
+                .catch(e => {
+                    hasError = true;
+                    if (e.response.status === 401) {
+                        this.errorMessage = "Does not posses the required rights to perform this action";
+                    }
+                    else if (e.response.status == 400) {
+                        this.errorMessage = e.response.data[0].errorMessage;
+                    }
+                    else {
+                        this.errorMessage = e.message;
+                    }
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+            $('#updatePlatformModal').modal('hide');
+            this.resetPlatformModels();
+            this.showPlatforms();
+        },
+
+
+        showDeleteModal: function (platform) {
+            $('#deletePlatformConfirmModal').modal('show');
+            this.selectedPlatform = platform;
+        },
+        showUpdateModal: function (platform) {
+            $('#updatePlatformModal').modal('show');
+            this.selectedPlatform = platform;
+            this.updatePlatformModel.name = platform.name;
+            this.updatePlatformModel.id = platform.id;
+        },
+
+        showCreateModal: function () {
+            $('#createPlatformModal').modal('show');
+        },
+
+        cancelPlatformAction: function () {
+            this.resetPlatformModels();
+        },
+
+        resetPlatformModels: function () {
+            this.newPlatformModel = {
+                name: "",
+                creationDate: new Date(),
+            };
+            this.updatePlatformModel = {
+                name: "",
+                platformId: 0,
+                creationDate: new Date(),
+            };
         }
+
     },
 });
