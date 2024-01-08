249912b52c2625005e4c037368d074044a8c78d7
JarnoHowest <Jarno.caenepeel@student.howest.be>
2024-01-08 15:30:48 +0100

    Check for date in the past

--- Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Request/DevelopersCreateDto.cs
+++ Pri.GameLibrary/Pri.GameLibrary.Api/DTOs/Request/DevelopersCreateDto.cs
@@ -6,6 +6,19 @@ namespace Pri.GameLibrary.Api.DTOs.Request
     {
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage ="Please select a date")]
+        [DateValid(ErrorMessage = "Date must be in the past")]
         public DateTime CreationDate { get; set; }
+
+        public class DateValidAttribute : ValidationAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                var date = (DateTime)value;
+                return date <= DateTime.Now;
+            }
+        }
     }
+    
 }
